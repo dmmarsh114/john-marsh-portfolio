09e64ab23879e956566c906b9a20adee6d7ee5b0
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-06-17 16:06:25 -0400

    added Fixing Up information

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,52 +9,28 @@ const About = (props) => {
 
     return (
         <div className='about' id='AboutTheAuthor'>
-            <Container>
-                <Row>
-                    <Col>
-                        <h2>About the Author</h2>
-                        <img src={authorPlaceholder} alt="author" />
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio eligendi in magni, vitae eum accusantium delectus ipsam tempora accusamus, tempore molestiae vero pariatur impedit numquam cumque saepe. A voluptas doloremque, quam obcaecati eaque laudantium nobis. Amet deleniti corrupti in saepe, blanditiis aliquid itaque animi tempora velit veniam iusto iste?
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio eligendi in magni, vitae eum accusantium delectus ipsam tempora accusamus, tempore molestiae vero pariatur impedit numquam cumque saepe. A voluptas doloremque, quam obcaecati eaque laudantium nobis. Amet deleniti corrupti in saepe, blanditiis aliquid itaque animi tempora velit veniam iusto iste?
-                        </p>
-                    </Col>
-                    <Col>
-                        <h2>Also By John Marsh</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus corrupti vel vitae exercitationem. Dolores rem eaque praesentium aperiam? Nisi ipsum necessitatibus maxime est soluta voluptatibus ut corporis architecto ipsa nesciunt.
-                        </p>
-                        <div>
-                            <h6>News</h6>
-                            <a href="">Grandma Article</a>
-                        </div>
-                        <div>
-                            <h6>Dewey Vids</h6>
-                            <ol>
-                                <li>
-                                    <a href="">Video 1</a>
-                                </li>
-                                <li>
-                                    <a href="">Video 2</a>
-                                </li>
-                            </ol>
-                        </div>
-                        <div>
-                            <h6>Public Speaking</h6>
-                            <a href="">Click here for booking</a>
-                            <a href="">Click here to see a preview!</a>
-                        </div>
-                        <div>
-                            <h6>Fixing Up</h6>
-                            <p>A comedy play by John and Jennifer Marsh</p>
-                        </div>
-                        <h6>Want to book me for speaking? Or license my play? Or just chat? Click below.</h6>
-                        <Button style={{ marginBottom: '10px' }} color='info'>Contact</Button>
-                    </Col>
-                </Row>
-            </Container>
+            <h2>About the Author</h2>
+            {/* About the Author ===================================== */}
+            <img src={authorPlaceholder} alt="author" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio eligendi in magni, vitae eum accusantium delectus ipsam tempora accusamus, tempore molestiae vero pariatur impedit numquam cumque saepe. A voluptas doloremque, quam obcaecati eaque laudantium nobis. Amet deleniti corrupti in saepe, blanditiis aliquid itaque animi tempora velit veniam iusto iste?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio eligendi in magni, vitae eum accusantium delectus ipsam tempora accusamus, tempore molestiae vero pariatur impedit numquam cumque saepe. A voluptas doloremque, quam obcaecati eaque laudantium nobis. Amet deleniti corrupti in saepe, blanditiis aliquid itaque animi tempora velit veniam iusto iste?</p>
+            <p><a href="">Click Here</a> to see John Marsh's other works.</p>
+
+            {/* Also by John Marsh ===================================== */}
+
+            {/* <div className='fixing-up'>
+                <div className='play-info'>
+                    <h5>Fixing Up</h5>
+                    <h6>A Comedy for the Stage in 2 Acts</h6>
+                    <p>by John and Jenni Marsh</p>
+                    <p><i>2014 McLaren Comedy Festival Winner</i></p>
+                </div>
+                <div className='play-quotes'>
+                    <p>“A wonderfully funny romantic farce”</p>
+                    <p>“A charming and splendorous comic affair”</p>
+                </div>
+                <p className='link'><a href="">Click Here</a> for a synopsis, excerpt and information on how to license this play</p>
+            </div> */}
         </div>
     )
 }
