af3a746e00791c2821e02de56011c62f1e155a31
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-06-17 14:53:28 -0400

    added basic cards with flipping animations

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './Components/About';
 import Footer from './Components/Footer';
 
 function App() {
+
   return (
     <div>
       <Header />
--- src/Components/BitsyStories.jsx
+++ src/Components/BitsyStories.jsx
@@ -14,80 +14,52 @@ const BitsyStories = () => {
     const collection1Sample = 'Collection 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia in eveniet itaque eaque tenetur ipsa enim similique vel, distinctio laborum architecto nam velit voluptate vero unde quas, blanditiis dignissimos neque!';
     const collection2Sample = 'Collection 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia in eveniet itaque eaque tenetur ipsa enim similique vel, distinctio laborum architecto nam velit voluptate vero unde quas, blanditiis dignissimos neque!';
 
-    const [sampleText, setSampleText] = React.useState('');
-    const [sampleModal, setSampleModal] = React.useState(false);
+    const [cardFlip, setCardFlip] = React.useState(false);
 
-    const toggle = () => setSampleModal(!sampleModal);
+    const flipCard = (id) => {
+        let card = document.getElementById(id);
 
-    const openSampleModal = (text) => {
-        setSampleText(text);
-        toggle();
+        if (cardFlip === false) {
+            card.style.transform = 'rotateY(-180deg)';
+            setCardFlip(true);
+        } else {
+            card.style.transform = 'initial';
+            setCardFlip(false);
+        }
     }
 
     return (
         <div className='bitsyStories' id='BitsyStories'>
             <h2>The Stories</h2>
 
-            <CardColumns className='bitsy-collections-container'>
+            {/* BOOK 1 ================================= */}
+            <div className="card-wrapper">
+                <h4>Book 1</h4>
+                <div className="card" id='book1'>
+                    <div className="card-front">
+                        <img src={collectionPic} style={{ width: '100%', height: '100%' }} />
+                    </div>
+                    <div className="card-back">
+                        <p>back content</p>
+                    </div>
+                </div>
+                <button onClick={() => flipCard('book1')}>click me</button>
+            </div>
 
-                <Card className='bitsy-collection collection-1 text-left'>
-                    <Row>
-                        <Col className='pic-container'>
-                            <CardImg className='collection-pic' src={collectionPic} alt='Collection1 Cover Art' />
-                        </Col>
-                        <Col>
-                            <CardBody>
-                                <CardTitle>Collection 1</CardTitle>
-                                <CardText>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore exercitationem aliquam sunt cum odit? Praesentium architecto tempore sed voluptates odio id provident laudantium. Nemo nam dicta saepe, iusto alias ea.
-                                </CardText>
-                                <CardText>
-                                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, est." -- from <i>Bitsy and the Lorem Ipusm</i>
-                                </CardText>
-                                <CardText>
-                                    With stories such as: <i>Lorem ipsum dolor, sit amet</i>, <i>consectetur adipisicing elit</i>, <i>Commodi voluptatem</i>
-                                </CardText>
-                                <Button color='info' onClick={() => openSampleModal(collection1Sample)}>Read A Sample</Button>
-                                <Button color='info'>Buy Now!</Button>
-                            </CardBody>
-                        </Col>
-                    </Row>
-                </Card>
+            {/* BOOK 2 ================================= */}
+            <div className="card-wrapper">
+                <h4>Book 2</h4>
+                <div className="card" id='book2'>
+                    <div className="card-front">
+                        <img src={collectionPic} style={{ width: '100%', height: '100%' }} />
+                    </div>
+                    <div className="card-back">
+                        <p>back content</p>
+                    </div>
+                </div>
+                <button onClick={() => flipCard('book2')}>click me</button>
+            </div>
 
-                <Card className='bitsy-collection collection-2 text-right'>
-                    <Row>
-                        <Col>
-                            <CardBody>
-                                <CardTitle>Collection 2</CardTitle>
-                                <CardText>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore exercitationem aliquam sunt cum odit? Praesentium architecto tempore sed voluptates odio id provident laudantium. Nemo nam dicta saepe, iusto alias ea.
-                                </CardText>
-                                <CardText>
-                                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, est." -- from <i>Bitsy and the Something-or-Other</i>
-                                </CardText>
-                                <CardText>
-                                    With stories such as: <i>Lorem ipsum dolor, sit amet</i>, <i>consectetur adipisicing elit</i>, <i>Commodi voluptatem</i>
-                                </CardText>
-                                <Button color='info'>Buy Now!</Button>
-                                <Button color='info' onClick={() => openSampleModal(collection2Sample)}>Read A Sample</Button>
-                            </CardBody>
-                        </Col>
-                        <Col className='pic-container'>
-                            <CardImg className='collection-pic' src={collectionPic} alt='Collection2 Cover Art' />
-                        </Col>
-                    </Row>
-                </Card>
-
-            </CardColumns>
-
-            <Modal isOpen={sampleModal} toggle={toggle}>
-                <ModalHeader toggle={toggle}>Sample Modal</ModalHeader>
-                <ModalBody>{sampleText}</ModalBody>
-                <ModalFooter>
-                    <Button color='danger' onClick={toggle}>Close</Button>
-                    <Button color='info'>Buy Here!</Button>
-                </ModalFooter>
-            </Modal>
         </div >
     )
 }
