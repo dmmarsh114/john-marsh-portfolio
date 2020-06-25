79dd65f5d7e3171d1e7907a371871d942490772b
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-06-25 12:12:04 -0400

    added public speaking and article copy

--- src/Components/AlsoBy.jsx
+++ src/Components/AlsoBy.jsx
@@ -13,7 +13,7 @@ const AlsoBy = (props) => {
     return (
         <div className='also-by'>
 
-            <a href="/"><Button color='info'>Go back</Button></a>
+            <a href="/" style={{ margin: '15px' }}><Button color='info'>Go back</Button></a>
 
             <h2>Also By John Marsh</h2>
             <hr style={{ borderColor: 'rgb(23,162,184)', width: '70%', marginBottom: '50px' }} />
@@ -49,9 +49,9 @@ const AlsoBy = (props) => {
                         <Col>
                             <h5>Dewey Shorts</h5>
                             <p>Professor Dewey B. Leevum (played by John Marsh) tries to make a name for himself, with some decidedly unorthodox views, in these short, funny mockumentaries. Where would you like to see Dewey go next?</p>
+                            <p>Filmed and edited by <a href="https://vimeo.com/user32093438" target='blank'>Terry Marsh</a></p>
                             <p><a href={dewey1} target='blank'>Space Aliens in Ancient Egypt – with Dewey B. Leevum</a></p>
                             <p><a href={dewey2} target='blank'>Dewey Does Time</a></p>
-                            <p>Filmed and edited by <a href="https://vimeo.com/user32093438" target='blank'>Terry Marsh</a></p>
                         </Col>
                     </Row>
                 </Container>
@@ -62,9 +62,21 @@ const AlsoBy = (props) => {
                     <Row>
                         <Col>
                             <h5>Public Speaking</h5>
-                            <p>John Marsh is also available for public speaking. You can book him for topics such as Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quaerat quibusdam iusto nihil et deleniti corporis repellendus tempore minima officia quod dolore laboriosam amet animi quisquam, quia dignissimos fugiat dolorum.</p>
+                            <p>Need an engaging and entertaining speaker for your event? John Marsh delivers lively talks on a variety of interesting topics for a wide range of audiences. His historical presentations fascinate audiences with their fresh stories and timely perspectives. Topics include subjects from American history from colonial times through today, as well as select topics in world history and world religions. He also presents history from biblical times for churches wishing to enrich their understanding with historical context. Marsh’s messages are ultimately positive looks at America and at human endeavors, and resonate extremely well with audiences where a love of freedom is present.</p>
                             <p><a href="">See a sample</a></p>
                             <p><a href="">Book him here</a></p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section className='articles'>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h5>Grandma Article</h5>
+                            <p>Here’s an article I wrote recently about teaching during a pandemic – a century ago. It is based on letters from my grandmother, who was a 21-year-old school teacher in 1918 during the Spanish Influenza pandemic. She has some very interesting things to say to us.</p>
+                            <p><a href="https://historynewsnetwork.org/article/175189" target='blank'>Read it here</a></p>
                         </Col>
                     </Row>
                 </Container>
