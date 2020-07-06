30d7a333458d3dd475c50810906073bafca5a283
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-07-06 12:15:03 -0400

    minor changes

--- src/Components/ContactModal.jsx
+++ src/Components/ContactModal.jsx
@@ -14,25 +14,15 @@ const ContactModal = (props) => {
     return (
         <Modal isOpen={props.modal} toggle={props.toggle} className='contactModal'>
             <ModalHeader toggle={props.toggle}>Contact Me</ModalHeader>
-            <Form>
+            <Form action='https://formspree.io/jsmarsh555@gmail.com' method='POST'>
                 <ModalBody>
-                    <Row className='name'>
-                        <Col>
-                            <FormGroup className='fname'>
-                                <Label for='fname'>First Name</Label>
-                                <Input id='fname' />
-                            </FormGroup>
-                        </Col>
-                        <Col>
-                            <FormGroup className='lname'>
-                                <Label for='lname'>Last Name</Label>
-                                <Input id='lname' />
-                            </FormGroup>
-                        </Col>
-                    </Row>
+                    <FormGroup>
+                        <Label for='name'>Full Name</Label>
+                        <Input id='name' name='name' required />
+                    </FormGroup>
                     <FormGroup>
                         <Label for='email'>Email</Label>
-                        <Input id='email' />
+                        <Input id='email' name='_replyto' required />
                     </FormGroup>
                     <FormGroup>
                         <Label for='subject'>Subject</Label>
@@ -40,20 +30,21 @@ const ContactModal = (props) => {
                             subject === 'Other' ?
                                 <InputGroup>
                                     <InputGroupAddon type='prepend'><Button outline='secondary' onClick={() => setSubject('')}>Back</Button></InputGroupAddon>
-                                    <Input id='subject' placeholder='Subject' />
+                                    <Input id='subject' placeholder='Subject' required />
                                 </InputGroup>
                                 :
-                                <Input type='select' id='subject' onChange={e => setSubject(e.target.value)}>
+                                <Input type='select' id='subject' required onChange={e => setSubject(e.target.value)}>
                                     <option disabled selected>Select One</option>
                                     <option>Fan Mail</option>
                                     <option>Licensing</option>
                                     <option>Other</option>
                                 </Input>
                         }
+                        <input type='hidden' name='_subject' value={'Contact Form Submission: ' + subject}></input>
                     </FormGroup>
                     <FormGroup>
                         <Label for='message'>Message</Label>
-                        <Input type='textarea' id='message' />
+                        <Input type='textarea' id='message' name='message' />
                     </FormGroup>
                 </ModalBody>
                 <ModalFooter>
