2ea4d0370fee5588b3d2a8e4faf3185023576452
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-07-21 11:39:16 -0400

    corrected typo

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,7 +9,7 @@ const About = (props) => {
         <div className='about' id='AboutTheAuthor'>
             <h2>About the Author</h2>
             <img src={authorImg} alt="author" />
-            <p>“When you have the real-life Bitsy for inspiration, these stories practically write themselves,” says John Marsh of his 2 collections of humorous stories inspired by his delightfully quirky and spirited wife, Jenni. The couple are pictured here getting a 25th Anniversary biplane ride. Together they wrote the hilarious farce, Fixing Up, which won the 2014 McLaren Comedy Playwriting Competition.</p>
+            <p>“When you have the real-life Bitsy for inspiration, these stories practically write themselves,” says John Marsh of his collection of humorous stories inspired by his delightfully quirky and spirited wife, Jenni. The couple are pictured here getting a 25th Anniversary biplane ride. Together they wrote the hilarious farce, Fixing Up, which won the 2014 McLaren Comedy Playwriting Competition.</p>
             <p>John is also an award-winning teacher, author, performer. Colorful story-telling and ample amounts of humor are hallmarks of his popular history classes. He is the winner of numerous teaching honors, including the inaugural Robert P. Bell Creative Teaching Award.</p>
             <p> John and Jenni live in Muncie, Indiana and have 3 children.</p>
             <p><a href="/alsoby">Click Here</a> to see some of John Marsh's other works.</p>
