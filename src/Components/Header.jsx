14d7042adb5d5bd25fd1498a69ac98465392eac5
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-07-22 11:48:35 -0400

    header responsiveness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,25 +2,18 @@ import React from 'react';
 import './css/header.css';
 
 import bitsyWagon from '../Assets/bitsy-wagon.png';
-import bitsyCameo from '../Assets/Bitsy-Cameo.png';
 
 import { Button, ButtonGroup, Container, Row, Col } from 'reactstrap';
-
-const amazonLink = 'https://www.amazon.com/Comic-Misadventures-Bitsy-John-Marsh/dp/B08C97X2CG/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=&sr=';
-
-// cached here for development, remove later pls
-// const headerPic = bitsyCameo;
-const headerPic = bitsyWagon;
 
 const Header = () => {
 
     return (
         <div className='header'>
             <div className='header-text'>
-                <div className='card'>
+                <div className='titleCard'>
                     <h4 className='subtitle'>Introducing a Delightful Collection of Hilarious Short Stories…</h4>
                     <br />
-                    <h1 className='title'><a href={amazonLink}>The Comic Misadventures of Bitsy</a></h1>
+                    <h1 className='title'>The Comic Misadventures of Bitsy</h1>
                 </div>
                 <br />
                 <br />
@@ -30,7 +23,7 @@ const Header = () => {
                 <a href="#AboutTheAuthor"><Button size='lg' color='info'>About the Author</Button></a>
             </ButtonGroup>
             <div style={{ width: '50%', float: 'right' }}>
-                <img src={headerPic} alt="Hero" />
+                <img src={bitsyWagon} alt="Hero" />
             </div>
         </div>
     )
