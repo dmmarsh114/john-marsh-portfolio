f9cf7db99738ff61dbd74e48de24fe1f33e52316
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-07-22 12:53:36 -0400

    image responsiveness

--- src/Components/AlsoBy.jsx
+++ src/Components/AlsoBy.jsx
@@ -3,7 +3,7 @@ import './css/alsoBy.css';
 
 import ContactModal from './ContactModal';
 
-import { Button, Container, Row, Col } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 import fixingUpLogo from '../Assets/FixingUpLogo.png';
 import deweyImg from '../Assets/dewey.png';
@@ -25,83 +25,51 @@ const AlsoBy = (props) => {
             <h1>Also By John Marsh</h1>
             <hr style={{ borderColor: 'var(--cover-orange)', width: '70%', marginBottom: '50px' }} />
 
-            <section className='fixing-up product-card'>
-                <Container>
-                    <Row>
-                        <Col>
-                            <div className='play-info'>
-                                <h2>Fixing Up</h2>
-                                <h6>A Comedy for the Stage in 2 Acts</h6>
-                                <p>by John and Jenni Marsh</p>
-                                <p><i>2014 McLaren Comedy Festival Winner</i></p>
-                            </div>
-                            <div className='play-quotes'>
-                                <p>“A wonderfully funny romantic farce”</p>
-                                <p>“A charming and splendorous comic affair”</p>
-                            </div>
-                            <span className='info-button' onClick={toggle}>Click Here</span><span> for a synopsis, excerpt and information on how to license this play.</span>
-                        </Col>
-                        <Col>
-                            <div className='play-logo'>
-                                <img src={fixingUpLogo} alt="Fixing Up" />
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+            <section className='fixing-up alsoby-card'>
+                <img src={fixingUpLogo} alt="Fixing Up" />
+
+                <div className='play-info'>
+                    <h2>Fixing Up</h2>
+                    <h6>A Comedy for the Stage in 2 Acts</h6>
+                    <p>by John and Jenni Marsh</p>
+                    <p><i>2014 McLaren Comedy Festival Winner</i></p>
+                </div>
+                <div className='play-quotes'>
+                    <p>“A wonderfully funny romantic farce”</p>
+                    <p>“A charming and splendorous comic affair”</p>
+                </div>
+                <span className='info-button' onClick={toggle}>Click Here</span><span> for a synopsis, excerpt and information on how to license this play.</span>
             </section>
 
-            <section className='deweyBLeevum product-card'>
-                <Container>
-                    <Row>
-                        <Col>
-                            <h2>Dewey Shorts</h2>
-                            <p>Professor Dewey B. Leevum (played by John Marsh) tries to make a name for himself, with some decidedly unorthodox views, in these short, funny mockumentaries. Where would you like to see Dewey go next?</p>
-                            <p>Filmed and edited by <a href="https://vimeo.com/user32093438" target='blank'>Terry Marsh</a></p>
-                            <p><a href={dewey1} target='blank'>Space Aliens in Ancient Egypt – with Dewey B. Leevum</a></p>
-                            <p><a href={dewey2} target='blank'>Dewey Does Time</a></p>
-                        </Col>
-                        <Col>
-                            <div className='play-logo'>
-                                <img src={deweyImg} alt="dewey b leevum" />
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+            <section className='deweyBLeevum alsoby-card'>
+                <img src={deweyImg} id='deweyImg' alt="Dewey B Leevum" />
+
+                <h2>Dewey Shorts</h2>
+                <p>Professor Dewey B. Leevum (played by John Marsh) tries to make a name for himself, with some decidedly unorthodox views, in these short, funny mockumentaries. Where would you like to see Dewey go next?</p>
+                <p>Filmed and edited by <a href="https://vimeo.com/user32093438" target='blank'>Terry Marsh</a></p>
+                <p><a href={dewey1} target='blank'>Space Aliens in Ancient Egypt – with Dewey B. Leevum</a></p>
+                <p><a href={dewey2} target='blank'>Dewey Does Time</a></p>
+            </section >
+
+            <section className='articles alsoby-card'>
+                <img src={francesDyke} alt="Frances Dyke" />
+                {/* <span style={{ clear: 'right' }}></span> */}
+
+                <h2>Teaching During a Pandemic – a Century Ago</h2>
+                <p>Here’s an article I wrote recently about teaching during a pandemic – a century ago. It is based on letters from my grandmother, who was a 21-year-old school teacher in 1918 during the Spanish Influenza pandemic. She has some very interesting things to say to us.</p>
+                <p><a href="https://historynewsnetwork.org/article/175189" target='blank'>Read it here</a></p>
             </section>
 
-            <section className='articles product-card'>
-                <Container>
-                    <Row>
-                        <Col>
-                            <h2>Teaching During a Pandemic – a Century Ago</h2>
-                            <p>Here’s an article I wrote recently about teaching during a pandemic – a century ago. It is based on letters from my grandmother, who was a 21-year-old school teacher in 1918 during the Spanish Influenza pandemic. She has some very interesting things to say to us.</p>
-                            <p><a href="https://historynewsnetwork.org/article/175189" target='blank'>Read it here</a></p>
-                        </Col>
-                        <Col>
-                            <div className='play-logo'>
-                                <img src={francesDyke} alt="Frances Dyke" />
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-
-            <section className='public-speaking product-card'>
-                <Container>
-                    <Row>
-                        <Col>
-                            <h2>Public Speaking</h2>
-                            <p>Need an engaging and entertaining speaker for your event? John Marsh delivers lively talks on a variety of interesting topics for a wide range of audiences. His historical presentations fascinate audiences with their fresh stories and timely perspectives. Topics include subjects from American history from colonial times through today, as well as select topics in world history and world religions. He also presents history from biblical times for churches wishing to enrich their understanding with historical context. Marsh’s messages are ultimately positive looks at America and at human endeavors, and resonate extremely well with audiences where a love of freedom is present.</p>
-                            <p className='info-button' onClick={toggle}>Book him here</p>
-                        </Col>
-                    </Row>
-                </Container>
+            <section className='public-speaking alsoby-card'>
+                <h2>Public Speaking</h2>
+                <p>Need an engaging and entertaining speaker for your event? John Marsh delivers lively talks on a variety of interesting topics for a wide range of audiences. His historical presentations fascinate audiences with their fresh stories and timely perspectives. Topics include subjects from American history from colonial times through today, as well as select topics in world history and world religions. He also presents history from biblical times for churches wishing to enrich their understanding with historical context. Marsh’s messages are ultimately positive looks at America and at human endeavors, and resonate extremely well with audiences where a love of freedom is present.</p>
+                <p className='info-button' onClick={toggle}>Book him here</p>
             </section>
 
             <a href="/" style={{ margin: '15px', marginBottom: '50px' }}><Button color='info'>Go back</Button></a>
 
             <ContactModal toggle={toggle} modal={contactModal} />
-        </div>
+        </div >
     );
 }
 
